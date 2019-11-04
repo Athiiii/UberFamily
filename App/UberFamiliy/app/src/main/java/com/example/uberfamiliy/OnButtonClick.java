90c0e2fd1cfe55e190dc7c6900f92010fc476be6
SeverinHersche <35527435+SeverinHersche@users.noreply.github.com>
2019-11-04 16:55:54 +0100

    made onclick method

---
+++ App/UberFamiliy/app/src/main/java/com/example/uberfamiliy/OnButtonClick.java
@@ -0,0 +1,11 @@
+package com.example.uberfamiliy;
+
+import android.view.View;
+
+public class OnButtonClick implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+        System.out.println("YEH");
+    }
+}
+
