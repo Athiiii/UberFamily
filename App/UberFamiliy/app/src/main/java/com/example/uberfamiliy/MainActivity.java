49b50241309ae9a3debd122f2ce1a12d2921764e
SeverinHersche <35527435+SeverinHersche@users.noreply.github.com>
2019-11-05 11:49:44 +0100

    Added the eevent to another Window

--- App/UberFamiliy/app/src/main/java/com/example/uberfamiliy/MainActivity.java
+++ App/UberFamiliy/app/src/main/java/com/example/uberfamiliy/MainActivity.java
@@ -2,21 +2,31 @@ package com.example.uberfamiliy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-import com.example.uberfamiliy.Actions.OnButtonClick;
+import com.example.uberfamiliy.Actions.OnPickMeUpClick;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button pickUpBtn = findViewById(R.id.buttonPickUP);
-        pickUpBtn.setOnClickListener(new OnButtonClick());
+        pickUpBtn.setOnClickListener(new OnPickMeUpClick());
+        View addFriendIMG = findViewById(R.id.addFriend);
+        addFriendIMG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddFriendWindow();
+            }
+        });
+    }
+    private void openAddFriendWindow(){
+        Intent addFriendWindow = new Intent(this, AddFriend.class);
+        startActivity(addFriendWindow);
     }
 
 
