1d18225fce936864985ba9d41c58883d3245c982
SeverinHersche <35527435+SeverinHersche@users.noreply.github.com>
2019-11-06 13:21:32 +0100

    Made ListView of users

--- App/UberFamiliy/app/src/main/java/com/example/uberfamiliy/AddFriendActivity.java
+++ App/UberFamiliy/app/src/main/java/com/example/uberfamiliy/AddFriendActivity.java
@@ -17,31 +17,28 @@ public class AddFriendActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-
-
+        ConnectToServer connectToServer = ConnectToAPI.getInstance();
+        fillUpList(connectToServer.getAllUsers());
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
-        //EditText searchText = findViewById(R.id.searchText);
-        //searchText.getText();
-        ConnectToServer connectToServer = ConnectToAPI.getInstance();
-        fillUpList(connectToServer.getAllUsers());
-
     }
 
     public void setHeader(ListView listView) {
         TextView textView = new TextView(this);
-        textView.setText("Hello. I'm a header view");
-
+        textView.setText(R.string.username);
+        textView.setTextSize(15);
         listView.addHeaderView(textView);
     }
 
     private void fillUpList(List<User> users) {
-        ArrayAdapter<User> userArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_single_choice, users);
         ListView userListView = findViewById(R.id.userList);
+        setHeader(userListView);
+
+        ArrayAdapter<User> userArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_single_choice, users);
         userListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         userListView.setAdapter(userArrayAdapter);
     }
