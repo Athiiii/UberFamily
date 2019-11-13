470c3576961189a364dc978f5401571630a48464
SeverinHersche <35527435+SeverinHersche@users.noreply.github.com>
2019-11-13 13:35:46 +0100

    socket done

---
+++ App/UberFamiliy/app/src/main/java/com/example/uberfamiliy/ShowAddressActivity.java
@@ -0,0 +1,14 @@
+package com.example.uberfamiliy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShowAddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_address);
+    }
+}
