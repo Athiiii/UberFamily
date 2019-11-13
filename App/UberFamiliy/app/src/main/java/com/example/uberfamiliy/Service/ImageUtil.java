23150524377f1b1abb64140a8803230b62f3abce
Athavan Theivakulasingham <44317288+Athiiii@users.noreply.github.com>
2019-11-13 08:34:34 +0100

    shrink bitmap

--- App/UberFamiliy/app/src/main/java/com/example/uberfamiliy/Service/ImageUtil.java
+++ App/UberFamiliy/app/src/main/java/com/example/uberfamiliy/Service/ImageUtil.java
@@ -3,22 +3,22 @@ package com.example.uberfamiliy.Service;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Base64;
+
 import java.io.ByteArrayOutputStream;
 
-public class ImageUtil
-{
-    public static Bitmap convert(String base64Str) throws IllegalArgumentException
-    {
+public class ImageUtil {
+    public static Bitmap convert(String base64Str) throws IllegalArgumentException {
         byte[] decodedBytes = Base64.decode(
-                base64Str.substring(base64Str.indexOf(",")  + 1),
+                base64Str.substring(base64Str.indexOf(",") + 1),
                 Base64.DEFAULT
         );
 
         return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
     }
 
-    public static String convert(Bitmap bitmap)
-    {
+    public static String convert(Bitmap bitmap) {
+        float heightRef = (120f / bitmap.getWidth()) * bitmap.getHeight();
+        bitmap = Bitmap.createScaledBitmap(bitmap, 120, (int) heightRef, false);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
 
