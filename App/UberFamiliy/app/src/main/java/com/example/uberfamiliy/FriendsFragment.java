bc24224012e2563c1ce5d1c956ea4a5520eb7cfd
SeverinHersche <35527435+SeverinHersche@users.noreply.github.com>
2019-11-13 09:38:11 +0100

    made mange Friends Part 1

--- App/UberFamiliy/app/src/main/java/com/example/uberfamiliy/FriendsFragment.java
+++ App/UberFamiliy/app/src/main/java/com/example/uberfamiliy/FriendsFragment.java
@@ -1,10 +1,13 @@
 package com.example.uberfamiliy;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -14,6 +17,7 @@ import androidx.fragment.app.Fragment;
 import com.example.uberfamiliy.Adapter.OnTextChangedAdapter;
 import com.example.uberfamiliy.DBConnection.CallAPIResponse;
 import com.example.uberfamiliy.DBConnection.ConnectToDB;
+import com.example.uberfamiliy.Service.ConvertJSON;
 import com.example.uberfamiliy.Service.SQLLight;
 import com.example.uberfamiliy.model.User;
 
@@ -23,16 +27,33 @@ import java.util.List;
 public class FriendsFragment extends Fragment {
     private List<User> friends;
     private ListView friendsView;
+    private SQLLight sqlLight;
+    private View root;
+    private Button removeButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_friends, container, false);
-        ConnectToDB.getInstance().getApprovedFriends(SQLLight.getInstance().getId(this.getContext()), new CallAPIResponse() {
+
+        root = inflater.inflate(R.layout.fragment_friends, container, false);
+
+        init();
+
+        ConnectToDB.getInstance().getApprovedFriends(sqlLight.getId(this.getContext()), new CallAPIResponse() {
             @Override
             public void processFinish(String output) {
-
+                friends = ConvertJSON.getInstance().toFriends(output);
+                fillUpListView(friends);
             }
         });
+
+        friendsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                setButtonEnabled();
+                selectedUser = (User) (userListView).getAdapter().getItem(position);
+            }
+        });
+
         EditText searchText = root.findViewById(R.id.searchText);
         searchText.addTextChangedListener(new OnTextChangedAdapter() {
             @Override
@@ -44,7 +65,20 @@ public class FriendsFragment extends Fragment {
                 }
             }
         });
+
         return root;
+    }
+
+    private void init() {
+        if (sqlLight == null) {
+            sqlLight = SQLLight.getInstance();
+        }
+        if (friendsView == null) {
+            friendsView = root.findViewById(R.id.friendList);
+        }
+        if (removeButton == null) {
+            removeButton = root.findViewById(R.id.removeFriends);
+        }
     }
 
     private void filterList(String searchEntry) {
@@ -66,6 +100,18 @@ public class FriendsFragment extends Fragment {
         ArrayAdapter<User> userArrayAdapter = new ArrayAdapter<>(this.getContext(), android.R.layout.simple_list_item_single_choice, userList);
         friendsView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         friendsView.setAdapter(userArrayAdapter);
+    }
+
+    private void setButtonEnabled() {
+        addFriendBtn.setBackgroundTintList(ColorStateList.valueOf(getColor(R.color.Button)));
+        addFriendBtn.setClickable(true);
+        addFriendBtn.setEnabled(true);
+    }
+
+    private void setButtonDisabled() {
+        addFriendBtn.setBackgroundTintList(ColorStateList.valueOf(getColor(R.color.ButtonGreyedOut)));
+        addFriendBtn.setEnabled(false);
+        addFriendBtn.setClickable(false);
     }
 
 
