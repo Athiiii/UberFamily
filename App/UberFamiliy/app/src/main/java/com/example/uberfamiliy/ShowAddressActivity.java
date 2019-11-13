a65c47d12aef448749b365e6dbb4569f71e799ed
SeverinHersche <35527435+SeverinHersche@users.noreply.github.com>
2019-11-13 13:56:10 +0100

    made Socket

--- App/UberFamiliy/app/src/main/java/com/example/uberfamiliy/ShowAddressActivity.java
+++ App/UberFamiliy/app/src/main/java/com/example/uberfamiliy/ShowAddressActivity.java
@@ -1,6 +1,9 @@
 package com.example.uberfamiliy;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,13 @@ public class ShowAddressActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_address);
+        Intent main = new Intent();
+        Bundle extras = main.getExtras();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        String longitude = extras.getString("longitude");
+        String latitude = extras.getString("latitude");
+        TextView text = findViewById(R.id.coordinates);
+        text.setText("longitude: " + longitude + "\n" + "latitude: " + latitude);
+
     }
 }
